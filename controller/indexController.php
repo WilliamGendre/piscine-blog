0ea84a0e7694d7d082a0a159e8046de16c73c7fd
WilliamGendre <william.gendre@lapiscine.pro>
2024-07-16 12:21:17 +0200

    indexViewBdd

--- controller/indexController.php
+++ controller/indexController.php
@@ -2,4 +2,10 @@
 
 require_once('../config/config.php');
 
+// Permet de faire une requête Select sans parmètres
+$stmt = $pdo->query('SELECT * FROM articles');
+
+// Retourne dans un tableau tous les produits grace au fetchAll, à ne pas confondre avec le fetch
+$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 require_once('../template/page/indexView.php');
--- template/page/indexView.php
+++ template/page/indexView.php
@@ -1,5 +1,22 @@
 <?php require_once('../template/partiel/header.php'); ?>
 
-<?php  require_once('../template/partiel/main.php');?>
+<main id="mainIndexView">
+
+    <section id="articleBdd">
+        <?php
+
+        foreach($articles as $article){
+
+            echo "<article>";
+            echo "<h2>" . $article['title'] . "</h2>";
+            echo "<h3>" . $article['content'] . "</h3>";
+            echo "<p>" . $article['createdAt'] . "</p>";
+            echo "</article>";
+        }
+
+        ?>
+    </section>
+
+</main>
 
 <?php  require_once('../template/partiel/footer.php');?>
