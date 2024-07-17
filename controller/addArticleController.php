c0ffeefd89ec0644a8759a57443f9f955e7c4677
WilliamGendre <william.gendre@lapiscine.pro>
2024-07-17 13:48:49 +0200

    articleRepository nouvelle fonction

--- controller/addArticleController.php
+++ controller/addArticleController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once ('../config/config.php');
+require_once ('../model/articleRepository.php');
 
 class AddarticleController
 {
@@ -8,35 +9,14 @@ class AddarticleController
     function addArticle()
     {
 
-        // Instance de class pour connecter à la base de données
-        // Model
-
-        $dbConnexion = new DbConnexion();
-        $pdo = $dbConnexion->connect();
-
         // controller
         $title = 'Mercredi';
         $content = 'milieu de semaine';
         $date = "2024-07-17";
 
-        // créer une nouvelle donnée
-        // model
-        $sql = "INSERT INTO articles (title, content, createdAt) VALUE (:title, :content, :createdAt)";
-        $stmt = $pdo->prepare($sql);
+        $articleRepository = new articleRepository();
+        $articleRepository -> insert($title, $content, $date);
 
-        // Définir les paramètres à éxécuter
-
-        $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':content', $content);
-        $stmt->bindParam(':createdAt', $date);
-
-        // Executer la requête
-        // view
-        if($stmt->execute()){
-            echo "nouvel article ajouté avec succès";
-        }else{
-            echo "Erreur lors de l'ajout de l'article";
-        }
     }
 
 }
--- model/articleRepository.php
+++ model/articleRepository.php
@@ -15,4 +15,31 @@ class ArticleRepository{
 
         return $articles;
     }
+
+    public function insert($title, $content, $date){
+        // Instance de class pour connecter à la base de données
+        // Model
+
+        $dbConnexion = new DbConnexion();
+        $pdo = $dbConnexion->connect();
+
+        // créer une nouvelle donnée
+        // model
+        $sql = "INSERT INTO articles (title, content, createdAt) VALUE (:title, :content, :createdAt)";
+        $stmt = $pdo->prepare($sql);
+
+        // Définir les paramètres à éxécuter
+
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':createdAt', $date);
+
+        // Executer la requête
+        // view
+        if($stmt->execute()){
+            echo "nouvel article ajouté avec succès";
+        }else{
+            echo "Erreur lors de l'ajout de l'article";
+        }
+    }
 }
