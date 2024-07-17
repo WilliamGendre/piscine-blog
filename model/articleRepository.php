6215690937020cdfd4ba56aae1da5ba7a0d81bb0
WilliamGendre <william.gendre@lapiscine.pro>
2024-07-17 14:06:09 +0200

    consturct

--- model/articleRepository.php
+++ model/articleRepository.php
@@ -1,14 +1,20 @@
 <?php
 
 class ArticleRepository{
-    public function findArticles(){
+
+    private $pdo;
+
+    function __construct(){
 
         // Instance de class pour accèder à la base de donnée
         $dbConnexion = new DbConnexion();
-        $pdo = $dbConnexion->connect();
+        $this->pdo = $dbConnexion->connect();
+
+    }
+    public function findArticles(){
 
         // Permet de faire une requête Select sans parmètres
-        $stmt = $pdo->query('SELECT * FROM articles');
+        $stmt = $this->pdo->query('SELECT * FROM articles');
 
         // Retourne dans un tableau tous les produits grace au fetchAll, à ne pas confondre avec le fetch
         $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -19,15 +25,10 @@ class ArticleRepository{
     // Les paramètres de la fonction permettent de récupérer les valeures qui sont enregistrer dans le controlleur
     public function insert($title, $content, $date){
 
-        // Instance de class pour connecter à la base de données
-        // Model
-        $dbConnexion = new DbConnexion();
-        $pdo = $dbConnexion->connect();
-
         // créer une nouvelle donnée
         // model
         $sql = "INSERT INTO articles (title, content, createdAt) VALUE (:title, :content, :createdAt)";
-        $stmt = $pdo->prepare($sql);
+        $stmt = $this->pdo->prepare($sql);
 
         // Définir les paramètres à éxécuter
 
